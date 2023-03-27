9138277e917dc18499b36d130cb55da551ace329
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-03-27 19:53:40 +0100

    added the add ons files

--- src/script.js
+++ src/script.js
@@ -1,10 +1,22 @@
 const toggleBill = document.querySelector('.toggle_bill');
-const month = document.querySelector('.month');
-const year = document.querySelector('.year');
+month = document.querySelector('.month'),
+ year = document.querySelector('.year'),
+ arcade = document.querySelector('.arcade'),
+ advanced = document.querySelector('.advanced'),
+ pro = document.querySelector('.pro');
+
 
 toggleBill.addEventListener('click',()=>{
     toggleBill.classList.toggle('justify-end');
     year.classList.toggle('text-MarineBlue');
-    month.classList.toggle('text-LightGray')
+    month.classList.toggle('text-LightGray');
+    const monthly = document.querySelectorAll('.monthly');
+    monthly.forEach(e =>{
+      e.classList.toggle('hidden');
+    });
+    const yearly = document.querySelectorAll('.yearly');
+    yearly.forEach(e =>{
+      e.classList.toggle('hidden');
+    }) 
 
 });
